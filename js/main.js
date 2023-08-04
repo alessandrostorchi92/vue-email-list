85a8aae9c6eaa56fbd6af6cb3f15ddc54d17dfaf
alessandrostorchi92 <storchi.alle@gmail.com>
2023-08-04 11:55:34 +0200

    Aggiunta file axios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 "use strict"
 
 // Consegna: Attraverso l’apposita API di Boolean generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+Vue.createApp({
+    data() {
+
+        return {
+
+            emailRandom: []
+
+        }
+    },
+    methods: {
+        generateEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((axiosResp) => {
+                    this.emailRandom.push(result.data.response);
+                    console.log(axiosResp)
+                    console.log(axiosResp.data.response);
+                });
+        }
+    },
+
+    mounted() {
+
+        for (let i = 0; i < 10; i++) {
+            this.generateEmail();
+        }
+
+    }
+
+}).mount("#app")
